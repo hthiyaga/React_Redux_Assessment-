8e9718f61e8a7501a2dfc661ef15bf04aec8c1e2
hthiyaga <hthiyaga@cs.odu.edu>
2019-12-26 11:58:56 -0500

    Assessment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Search from './components/Search';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import store from './store';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>RTS_LABSs</p>
+        <Provider store={store}>
+          <Router>
+            <Fragment>
+              <Switch>
+                <Route exact path='/' component={Search} />
+              </Switch>
+            </Fragment>
+          </Router>
+        </Provider>
       </header>
     </div>
   );
